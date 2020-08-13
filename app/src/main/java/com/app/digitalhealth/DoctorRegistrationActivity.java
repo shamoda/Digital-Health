86b0cab7e8eb34a5db7b95767eae423b09776f4f
shamoda <it19161648@my.sliit.lk>
2020-08-13 11:00:47 +0530

    Doctor registration interface added.

---
+++ app/src/main/java/com/app/digitalhealth/DoctorRegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.app.digitalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_registration);
+    }
+}
