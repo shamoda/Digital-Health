204461c5e1f6058ad0b986225d328476de744c30
Senath99 <senathweerasingha@gmail.com>
2020-08-15 22:31:35 +0530

    Test Commit

---
+++ app/src/main/java/com/app/digitalhealth/InsulinDoseCal.java
@@ -0,0 +1,14 @@
+package com.app.digitalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsulinDoseCal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insulin_dose_cal);
+    }
+}
