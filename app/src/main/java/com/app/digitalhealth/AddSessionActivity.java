ea3bbeb64486c0e0d0c1637b4d1ebb54bd24d922
Senath99 <senathweerasingha@gmail.com>
2020-09-25 14:52:24 +0530

    appointment/sessions Resolved

--- app/src/main/java/com/app/digitalhealth/AddSessionActivity.java
+++ app/src/main/java/com/app/digitalhealth/AddSessionActivity.java
@@ -15,7 +15,6 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 
-
 //import com.app.digitalhealth.model.Sessions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
